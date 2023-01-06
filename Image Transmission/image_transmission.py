14884db23420f8d1c7327e9d9f357c1f1eeacfa1
Sayatcha <god_wk@hotmail.com>
2023-01-07 02:01:32 +0800

    Sending Image Data

--- Image Transmission/image_transmission.py
+++ Image Transmission/image_transmission.py
@@ -9,26 +9,8 @@ import concurrent.futures
 url = 'http://192.168.10.162/cam-hi.jpg'
 im = None
 
-
-def run():
-    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
-    while True:
-        img_resp = urllib.request.urlopen(url)
-        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
-        im = cv2.imdecode(imgnp, -1)
-
-        bbox, label, conf = cv.detect_common_objects(im)
-        im = draw_bbox(im, bbox, label, conf)
-
-        cv2.imshow('detection', im)
-        key = cv2.waitKey(5)
-        if key == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
+img_resp = urllib.request.urlopen(url)
+imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+im = cv2.imdecode(imgnp, -1)
 
 
-if __name__ == '__main__':
-    print("started")
-    with concurrent.futures.ProcessPoolExecutor() as executer:
-        f = executer.submit(run)
